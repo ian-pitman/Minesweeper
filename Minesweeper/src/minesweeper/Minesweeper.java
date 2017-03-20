68aa30aa53b8c0bf849bf03cb2c3747c13097fb8
BuildTools <unconfigured@null.spigotmc.org>
2017-03-19 20:20:18 -0700

    GUI Skeleton

--- Minesweeper/src/minesweeper/Minesweeper.java
+++ Minesweeper/src/minesweeper/Minesweeper.java
@@ -1,33 +1,73 @@
 package minesweeper;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Minesweeper extends Application {
     
+    private MenuBar menu;
+    private Menu file;
+    private MenuItem beginner;
+    private MenuItem intermediate;
+    private MenuItem expert;
+    private MenuItem custom;
+    private Menu options;
+    private MenuItem setMines;
+    private MenuItem gridSize;
+    private Menu about;
+    private Menu howTo;
+    private Menu exit;
+    
+    private BorderPane root;
+    private HBox hud;
+    private Label time;
+    private Label mines;
+    
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        root = new BorderPane();
+        menu = new MenuBar();
+        hud = new HBox();
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        file = new Menu("File");
+        options = new Menu("Options");
+        about = new Menu("About");
+        howTo = new Menu("How To Play");
+        exit = new Menu("Exit");
         
-        Scene scene = new Scene(root, 300, 250);
+        beginner = new MenuItem("Start Beginner Game");
+        intermediate = new MenuItem("Start Intermediate Game");
+        expert = new MenuItem("Start Expert Game");
+        custom = new MenuItem("Start Custom Game");
         
-        primaryStage.setTitle("Hello World!");
+        setMines = new MenuItem("Set Mines");
+        gridSize = new MenuItem("Set Grid Size");
+        
+        file.getItems().addAll(beginner, intermediate, expert, custom);
+        options.getItems().addAll(setMines, gridSize);
+        menu.getMenus().addAll(file, options, about, howTo, exit);
+        
+        time = new Label("Time Elapsed: ");
+        mines = new Label("Mines Remaining: ");
+        hud.setPadding(new Insets(10, 10, 10, 10));
+        hud.setSpacing(10);
+        hud.setAlignment(Pos.CENTER);
+        hud.getChildren().addAll(time, mines);
+        
+        root.setTop(menu);
+        root.setBottom(hud);
+        Scene scene = new Scene(root, 800, 600);
+        
+        primaryStage.setTitle("Minesweeper GUI (Template)");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
